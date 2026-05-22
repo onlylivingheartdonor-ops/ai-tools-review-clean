3912ab30878dab5e5e0cdf2070401e947efaba8a
hsking1964 <onlylivingbraindonor@gmail.com>
2026-05-22 15:45:55 -0400

    Final push before Netlify deployment

--- app/layout.js
+++ app/layout.js
@@ -18,9 +18,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-	<meta name='impact-site-verification' value='3b8829bc-8f97-4931-b938-fc432ec8dfc2'>
+        <meta name="impact-site-verification" content="3b8829bc-8f97-4931-b938-fc432ec8dfc2" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3475627763908800" crossOrigin="anonymous"></script>
-        {/* Adsterra script will go here */}
       </head>
       <body>{children}</body>
     </html>
